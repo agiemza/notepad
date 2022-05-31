45eef8acf8349c30efbfb9adb33caec996c47265
Arkadiusz <arkadiusz@Arkadiuszs-MacBook-Pro.local>
2022-05-31 21:31:03 +0200

    Added creating new notes which are stored in state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,106 @@
-import { useEffect, useState } from "react"
+import { nanoid } from "nanoid"
+import { useState } from "react"
 import Toolbar from "./components/toolbar"
 
 export default function App() {
-    const [notes, setNotes] = useState("")
-    
+    const [notes, setNotes] = useState([])
+    const [currentNoteID, setCurrentNoteID] = useState(notes[0] && notes[0].id)
+
     function handleInput(e) {
-        setNotes(e.target.innerHTML.replace(/&nbsp;/g, ' ').replace(/&amp;/g, "&").replace(/&lt;/g, "<").replace(/&gt;/g, ">"))
+        const currentNote = notes.find(note => note.id === currentNoteID)
+        currentNote.content = e.target.innerHTML.replace(/&nbsp;/g, ' ').replace(/&amp;/g, "&").replace(/&lt;/g, "<").replace(/&gt;/g, ">")
+    }
+
+    function getTimeAndDate() {
+        const dateObject = new Date()
+        const hours = dateObject.getHours() < 10 ? `0${dateObject.getHours()}` : dateObject.getHours()
+        const minutes = dateObject.getMinutes() < 10 ? `0${dateObject.getMinutes()}` : dateObject.getMinutes()
+        const seconds = dateObject.getSeconds() < 10 ? `0${dateObject.getSeconds()}` : dateObject.getSeconds()
+
+        const day = dateObject.getDate() < 10 ? `0${dateObject.getDate()}` : dateObject.getDate()
+        let month
+        switch (dateObject.getMonth()) {
+            case 0:
+                month = "January"
+                break
+            case 1:
+                month = "February"
+                break
+            case 2:
+                month = "March"
+                break
+            case 3:
+                month = "April"
+                break
+            case 4:
+                month = "May"
+                break
+            case 5:
+                month = "June"
+                break
+            case 6:
+                month = "July"
+                break
+            case 7:
+                month = "August"
+                break
+            case 8:
+                month = "Septemper"
+                break
+            case 9:
+                month = "October"
+                break
+            case 10:
+                month = "November"
+                break
+            case 11:
+                month = "December"
+                break
+            default:
+                console.log("Error occured while loading month.")
+                break
+        }
+
+        const date = `${day} ${month} ${dateObject.getFullYear()}`
+        const time = `${hours}:${minutes}:${seconds}`
+
+        return { time, date }
+    }
+
+    function createNewNote() {
+        const newNoteID = nanoid()
+        const newNote = {
+            id: newNoteID,
+            title: "New Note",
+            content: "",
+            timeCreated: {
+                date: getTimeAndDate().date,
+                time: getTimeAndDate().time
+            },
+            timeEdited: {
+                date: getTimeAndDate().date,
+                time: getTimeAndDate().time
+            }
+        }
+        console.log(newNote)
+        setNotes(prevState => [...prevState, newNote])
+        setCurrentNoteID(newNoteID)
     }
 
     function handleTest() {
-
+        console.log(notes)
     }
+
     return (
         <section className="app">
             <button onClick={handleTest}>Test something</button>
             <div className="allnotes-container">
-
+                {/* {notes} */}
             </div>
             <div className="notepad-container">
-                <Toolbar />
+                <Toolbar
+                    createNewNote={createNewNote}
+                />
                 <div className="textArea" id="textArea" onInput={(e) => handleInput(e)} contentEditable suppressContentEditableWarning>
                 </div>
             </div>
--- src/components/toolbar.js
+++ src/components/toolbar.js
@@ -1,36 +1,39 @@
 
-export default function Toolbar() {
+export default function Toolbar(props) {
 
     function handleClick(property) {
         document.execCommand(property)
     }
-    
+
     return (
         <div className="toolbar">
-            <button
-                onClick={() => handleClick("bold")}
-                style={{ fontWeight: '700' }}
-            >
-                B
-            </button>
-            <button
-                onClick={() => handleClick("italic")}
-                style={{ fontStyle: 'italic' }}
-            >
-                I
-            </button>
-            <button
-                onClick={() => handleClick("underline")}
-                style={{ textDecoration: 'underline' }}
-            >
-                U
-            </button>
-            <button
-                onClick={() => handleClick("strikeThrough")}
-                style={{ textDecoration: 'line-through' }}
-            >
-                S
-            </button>
+            <button onClick={props.createNewNote}>New note</button>
+            <div className="stylingBar">
+                <button
+                    onClick={() => handleClick("bold")}
+                    style={{ fontWeight: '700' }}
+                >
+                    B
+                </button>
+                <button
+                    onClick={() => handleClick("italic")}
+                    style={{ fontStyle: 'italic' }}
+                >
+                    I
+                </button>
+                <button
+                    onClick={() => handleClick("underline")}
+                    style={{ textDecoration: 'underline' }}
+                >
+                    U
+                </button>
+                <button
+                    onClick={() => handleClick("strikeThrough")}
+                    style={{ textDecoration: 'line-through' }}
+                >
+                    S
+                </button>
+            </div>
         </div>
     )
 }
